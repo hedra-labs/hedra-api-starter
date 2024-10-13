24633b1e06bd7fc2e6e803751ccfb77fc0b09e29
Michael Lingelbach <m.j.lbach@gmail.com>
2024-10-13 05:30:47 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Hedra API Starter
+
+This repository shows how to use the Hedra API, which is currently in beta.
+
+# Dependencies
+
+
+# Examples
+
+## 16:9
+
+```bash
+python main.py --api-key sk_hedra-*** --audio assets/audio.wav --img assets/16_9.jpg --ar 16:9
+```
+
+## 9:16
+
+```bash
+python main.py --api-key sk_hedra-*** --audio assets/audio.wav --img assets/9_16.jpg --ar 9:16
+```
+
+## 1:1
+
+```bash
+python main.py --api-key sk_hedra-*** --audio assets/audio.wav --img assets/1:1.jpg --ar 1:1
+```
---
+++ main.py
@@ -0,0 +1,67 @@
+import requests
+from requests import Response
+import json
+import os
+import argparse
+from typing import Literal
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+BASE_URL = ' https://mercury.dev.dream-ai.com/api'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--api-key',required=True,)
+parser.add_argument('--img',required=True,)
+parser.add_argument('--audio',required=True,)
+parser.add_argument('--ar',required=True,)
+args = parser.parse_args()
+
+api_key = args.api_key
+assert api_key is not None and len(api_key) > 0, 'Missing API_KEY'
+
+headers={'X-API-KEY': args.api_key}
+
+
+def cprint(text, color: Literal['red', 'green', 'blue']):
+    for l in text.splitlines():
+        if color == 'red':
+            print("\033[91m {}\033[00m".format(l))
+        elif color == 'green':
+            print("\033[92m {}\033[00m".format(l))
+        else:
+            print("\033[96m {}\033[00m".format(l))
+
+
+def check_response(response: Response):
+    ok = response.status_code < 400
+    color = 'green' if ok else 'red'
+    cprint(f'{response.status_code}', color=color)
+    cprint(json.dumps(response.json(), indent=4), color=color)
+    
+    if not ok:
+        cprint('request failed, exit', color='red')
+        exit(1)
+
+
+with open(os.path.join(SCRIPT_DIR, args.audio),'rb') as f:
+    print(f'uploading {args.audio}')
+    audio_response = requests.post(f"{BASE_URL}/v1/audio", headers=headers, files={'file': f})
+    check_response(audio_response)
+
+with open(os.path.join(SCRIPT_DIR, args.img),'rb') as f:
+    print(f'uploading {args.img}')
+    image_response = requests.post(f"{BASE_URL}/v1/portrait", headers=headers, files={'file': f})
+    check_response(image_response)
+
+
+print('initiating video generation')
+video_response = requests.post(
+    f"{BASE_URL}/v1/characters",
+    headers=headers,
+    json={
+        "avatarImage": image_response.json()["url"], 
+        "audioSource": "audio", 
+        "voiceUrl": audio_response.json()["url"],
+        "aspectRatio": args.ar
+    }
+)
+check_response(video_response)

1f4494ee8dd4e250f3a80be99bd9d960a103c40f
Michael Lingelbach <m.j.lbach@gmail.com>
2024-10-13 05:38:06 -0700

    First commit

--- README.md
+++ README.md
@@ -2,25 +2,42 @@
 
 This repository shows how to use the Hedra API, which is currently in beta.
 
+We've included examples assets under the `assets` folder which are stored via `git-lfs`. You must install [git-lfs](https://git-lfs.com) prior to running the examples and cloning the repo or run `git lfs fetch`.
+
 # Dependencies
 
+* Python 3.10
+* requests (`pip install requests`)
+* `git-lfs`
+
+# Notes
+
+The Hedra API supports generating videos up to 4 minute videos. You provide either a 1:1, 16:9, or 9:16 `.jpg`, a `.wav`, and receive back an mp4 video.
+
+# Documentation
+
+Available [here](https://www.hedra.com/docs)
 
 # Examples
 
 ## 16:9
 
 ```bash
-python main.py --api-key sk_hedra-*** --audio assets/audio.wav --img assets/16_9.jpg --ar 16:9
+python main.py --api-key sk_hedra-*** --audio assets/audio.wav --img assets/16_9.jpg --ar 16:9 --canary
 ```
 
 ## 9:16
 
 ```bash
-python main.py --api-key sk_hedra-*** --audio assets/audio.wav --img assets/9_16.jpg --ar 9:16
+python main.py --api-key sk_hedra-*** --audio assets/audio.wav --img assets/9_16.jpg --ar 9:16 --canary
 ```
 
 ## 1:1
 
 ```bash
-python main.py --api-key sk_hedra-*** --audio assets/audio.wav --img assets/1:1.jpg --ar 1:1
+python main.py --api-key sk_hedra-*** --audio assets/audio.wav --img assets/1:1.jpg --ar 1:1 --canary
 ```
+
+# Questions?
+
+Contact support@hedra.com
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import os
 import argparse
 from typing import Literal
+import time
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 BASE_URL = ' https://mercury.dev.dream-ai.com/api'
@@ -13,12 +14,16 @@ parser.add_argument('--api-key',required=True,)
 parser.add_argument('--img',required=True,)
 parser.add_argument('--audio',required=True,)
 parser.add_argument('--ar',required=True,)
+parser.add_argument('--canary',action='store_true')
+
 args = parser.parse_args()
 
 api_key = args.api_key
 assert api_key is not None and len(api_key) > 0, 'Missing API_KEY'
 
 headers={'X-API-KEY': args.api_key}
+if args.canary:
+    headers["enable-canary"] = "True"
 
 
 def cprint(text, color: Literal['red', 'green', 'blue']):
@@ -41,7 +46,6 @@ def check_response(response: Response):
         cprint('request failed, exit', color='red')
         exit(1)
 
-
 with open(os.path.join(SCRIPT_DIR, args.audio),'rb') as f:
     print(f'uploading {args.audio}')
     audio_response = requests.post(f"{BASE_URL}/v1/audio", headers=headers, files={'file': f})
@@ -49,12 +53,12 @@ with open(os.path.join(SCRIPT_DIR, args.audio),'rb') as f:
 
 with open(os.path.join(SCRIPT_DIR, args.img),'rb') as f:
     print(f'uploading {args.img}')
-    image_response = requests.post(f"{BASE_URL}/v1/portrait", headers=headers, files={'file': f})
+    image_response = requests.post(f"{BASE_URL}/v1/portrait", headers=headers, files={'file': f}, params={"aspect_ratio": args.ar})
     check_response(image_response)
 
 
 print('initiating video generation')
-video_response = requests.post(
+project = requests.post(
     f"{BASE_URL}/v1/characters",
     headers=headers,
     json={
@@ -64,4 +68,24 @@ video_response = requests.post(
         "aspectRatio": args.ar
     }
 )
-check_response(video_response)
+check_response(project)
+
+project_id = project.json()['jobId']
+
+print('polling for response')
+while True:
+    video_response = requests.get(
+        f"{BASE_URL}/v1/projects/{project_id}",
+        headers=headers,
+    )
+    if video_response.json().get("status") == 'Completed':
+        break
+    time.sleep(5)
+
+
+print('fetching video')
+video_url = video_response.json().get("videoUrl")
+response = requests.get(video_url)
+
+with open(f'{project_id}.mp4', 'wb') as f: 
+    f.write(response.content)
